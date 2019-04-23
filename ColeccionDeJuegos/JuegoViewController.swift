b432ea0e4702c4f42a63fe8dac491c302cf7ee1a
Mario Fernando <mariofernando@MacBook-Air-de-Mac-3.local>
2019-04-23 17:47:36 -0500

    First

---
+++ ColeccionDeJuegos/JuegoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  JuegoViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by Mario Fernando on 23/04/19.
+//  Copyright © 2019 Tecsup. All rights reserved.
+//
+
+import UIKit
+
+class JuegoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
